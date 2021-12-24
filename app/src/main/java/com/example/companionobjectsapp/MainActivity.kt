b00e023ba1b558e1ee7bb14c5d2a8db13d3b302d
Zahrasaad44 <Zzsaad44@gmail.com>
2021-12-24 03:09:41 +0300

    Initial commit

---
+++ app/src/main/java/com/example/companionobjectsapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.companionobjectsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.companionobjectsapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        binding.submitBtn.setOnClickListener { applyChanges() }
+
+    }
+
+    private fun applyChanges() {
+        val input = binding.userInputET.text.toString().lowercase()
+        when (input) {
+            "day" -> Time.makeDay(binding.dayNightCL, binding.instructionTV, binding.userInputET, binding.submitBtn)
+            "night" -> Time.makeNight(binding.dayNightCL, binding.instructionTV, binding.userInputET, binding.submitBtn)
+            else -> Toast.makeText(this, "Type 'day' or 'night' only", Toast.LENGTH_LONG).show()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/companionobjectsapp/Time.kt
@@ -0,0 +1,27 @@
+package com.example.companionobjectsapp
+
+import android.graphics.Color
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+
+
+class Time {
+    companion object {
+
+        fun makeDay(layout: ConstraintLayout, text: TextView, field: EditText, btn: Button) {
+           layout.setBackgroundResource(R.drawable.day)
+            text.setTextColor(Color.BLACK)
+            field.setTextColor(Color.BLACK)
+            btn.setBackgroundColor(Color.parseColor("#F3A738"))
+        }
+
+        fun makeNight(layout: ConstraintLayout, text: TextView, field: EditText, btn: Button){
+            layout.setBackgroundResource(R.drawable.night)
+            text.setTextColor(Color.WHITE)
+            field.setTextColor(Color.WHITE)
+            btn.setBackgroundColor(Color.parseColor("#939CD1"))
+        }
+    }
+}
